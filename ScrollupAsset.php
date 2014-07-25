93136e6e6c351fb661f0b949b4d0ac9d474f9c23
raoul <raoul_boulard@hotmail.com>
2014-07-25 14:03:57 +0200

    Apply Yii2 coding Standard

--- ScrollupAsset.php
+++ ScrollupAsset.php
@@ -4,20 +4,28 @@ namespace raoul2000\widget\scrollup;
 use yii\web\AssetBundle;
 
 /**
+ *
  * @author Raoul <raoul.boulard@gmail.com>
  */
 class ScrollupAsset extends AssetBundle
 {
+
 	public $depends = [
-		'yii\web\JqueryAsset',
+		'yii\web\JqueryAsset'
 	];
-	public function init() {
-		$this->sourcePath = __DIR__.'/assets';
-		
-		if(defined('YII_DEBUG')){
-			$this->js = ['js/jquery.scrollUp.js'];
-		}else {
-			$this->js = ['js/jquery.scrollUp.min.js'];
+
+	public function init()
+	{
+		$this->sourcePath = __DIR__ . '/assets';
+
+		if (defined('YII_DEBUG')) {
+			$this->js = [
+				'js/jquery.scrollUp.js'
+			];
+		} else {
+			$this->js = [
+				'js/jquery.scrollUp.min.js'
+			];
 		}
 		return parent::init();
 	}
